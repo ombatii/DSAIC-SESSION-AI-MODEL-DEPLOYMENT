318c3d82ac794708d44959096fad8bab171ba294
Brian Ombati <109950763+ombatii@users.noreply.github.com>
2024-09-25 20:28:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('Deploying AI Model')
+st.title('😁DSAIC-Deploying-AI-model')
 
-st.write('Hello world!')
+st.info('This app builds a AI model on penguine dataset')
