0181695286b593263675d5fe57c798b9ed8a921b
Brian Ombati <109950763+ombatii@users.noreply.github.com>
2024-09-25 20:29:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,17 @@ from sklearn.ensemble import RandomForestClassifier
 st.title('😁DSAIC-Deploying-AI-model')
 
 st.info('This app builds a AI model on penguine dataset')
+
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('penguins_data.csv')
+  df
+
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
